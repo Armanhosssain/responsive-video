4027757251f2f140c71c60a305ea98d8fd42be37
Arman hossain <armanh686@gmail.com>
2019-10-21 00:55:05 +0600

    responsiv video

---
+++ responsive-video.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>simple blog</title>
+</head>
+<body>
+	<!-- start home video section -->
+  <section id="home-video" class="text-center text-light">
+    <div class="dark-overlay">
+      <div class="container">
+        <div class="row">
+          <div class="col pt-5 mt-4">
+            <div uk-lightbox>
+              <a href="https://youtu.be/43rTanJhQqM" class="text-light">
+                <i class="fa fa-play"></i>
+              </a>
+            </div>
+            <h3 class="mt-3">Bootstrap 4 Crash Course</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end home video section -->
+   <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+</body>
+</html>
